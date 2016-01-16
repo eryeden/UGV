698f907938c62101297210be7faa321a2247d0f1
Kazuki Kikuti <ery.eden@gmail.com>
2016-01-17 01:20:44 +0900

    Modify Launch path of STM32CubeMX

--- CubeMX.sh
+++ CubeMX.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-PATH2CUBEMX="/usr/local/STMicroelectronics/STM32Cube/STM32CubeMX"
-java -cp ${PATH2CUBEMX} com.st.microxplorer.maingui.IOConfigurator
+
+#起動パスがcom.st.microxplorer.maingui.STM32CubeMXに変更になった。
+#前のじゃ起動しないので注意
+PATH2CUBEMX="${HOME}/local_devel/lib/STM32CubeMX"
+java -cp ${PATH2CUBEMX} com.st.microxplorer.maingui.STM32CubeMX 
+
